bfb4cf0502d428161f7fc7bfdca20e018515cd53
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2022-11-14 16:50:58 +0800

    Update submission.js

--- .routes/submission.js
+++ .routes/submission.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const { boolean } = require("yargs");
+const router = express.Router();
+const db = require('../database');
 
+router.get("/", async (req, res) => {
+    res.render('submission');
+    await db.promise().query(`insert into submission values(1, 'your mom', 'your mom', 0, true, 'your mom', 'your mom')`);
+})
+
+router.post("/", async (req, res) => {
+    // const content = req.body;
+    // const results = await db.promise().query(`select * from submission`);
+    // const submissions = results[0];
+    // var currentID = results[0][-1].id;
+    // db.promise().query(`insert into submission values(currentID+1, '${content.techname}', '${content.description}', '${content.categories}', accepted, '${content.username}', '${content.contact}')`);
+    
+})
+
+module.exports = router
