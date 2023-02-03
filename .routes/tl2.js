ba1ced063f347ce07bd3547529e8292e2118405e
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2023-02-03 16:15:35 +0800

    Update tl2.js

--- .routes/tl2.js
+++ .routes/tl2.js
@@ -4,10 +4,23 @@ const db = require('../database');
 
 router.get("/", async (req, res) => {
     //get tech under tl1 category
-    //send in render
-    const content = await db.promise().query(`select * from submissions`);
-    var results = content[0];
-    console.log(results);
+    const subs = await db.promise().query(`select * from submissions`);
+    const tl2true = await db.promise().query(`select * from domains where U = true or MDL = true or RA = true or RoTech = true or LS = true`);
+    //list contains list of entrys with domains that are part of tl1
+    const list = tl2true[0];
+    var ids = [];
+    for(let i = 0; i<list.length; i++){
+        ids.push(list[i]["id"]);
+    }
+    var content = [];
+    for(let i = 0; i<subs[0].length; i++){
+        if(ids.includes(subs[0][i]["id"])){
+            content.push(subs[0][i]);
+        }
+    }
+    var results = [content, list];
+    console.log(ids)
+    console.log(content);
     res.render('tl2', {content: results});
 })
 
